5f4465c459e698bdda07fb2b2a03b3a6c5ed00dc
Bernardo A Mira García <bamira1@misena.edu.co>
2024-08-14 16:21:59 -0500

    init
    repo tuya

---
+++ ApuntesLogicaTuyaSA/src/ejercicios/HolaMundo.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public class HolaMundo {
+
+    public static void main(String [] args){
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ ApuntesLogicaTuyaSA/src/notasdeclase/Variable.java
@@ -0,0 +1,45 @@
+package notasdeclase;
+
+public class Variable {
+
+    public static void main(String [] args){
+
+        int idCliente;  //Declarar una variable
+
+        idCliente = 12345678; // Inicializar una variable
+
+        String nombreCliente = "Pepito";
+
+        int num1, num2;
+
+        num1 = 5;
+        num2 = 10;
+
+        byte edad = 127;
+        short mesada = 32767;
+        short ahorroMesada = mesada;
+
+        // Vamos a usar el casteo de variables
+
+        int ahorro = (int) ahorroMesada;
+
+        ahorro = ahorroMesada + mesada;
+
+        System.out.println(ahorro);
+        //Parsing o parseo de variables
+        //Saque el promedio de las edades
+        int edad1 = 19;
+        String edad2 = "22";
+        int edadConv2 = Integer.parseInt(edad2);
+        System.out.println(edadConv2);
+        float promedio = (edad1 + edadConv2)/2;
+
+        System.out.println(promedio);
+
+        String promedioConv = promedio + "";
+
+        System.out.println(promedioConv);
+
+
+    }
+}
